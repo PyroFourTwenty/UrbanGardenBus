595d519554fdae8ea08a8596bcaeefb7d3389e64
PyroFourTwenty <kermitthecreeper@gmail.com>
2022-09-23 17:05:17 +0200

    implemented GardenBusSensor object

---
+++ GardenBusClient/sensor.py
@@ -0,0 +1,38 @@
+from GardenBusClient.SupportedSensors import supported_sensors
+from . import gb_utils
+class GardenBusSensor():
+    sensor_name: str = None  #stores the model name of the sensor
+    sensor_model_id: int = None #
+    tags: list = None
+    calibration_required = None
+    calibration_value = None
+    get_value_function = None 
+
+
+    def __init__(self, sensor_model_id, tags: list, 
+            get_value_function, 
+            calibration_function = None,
+            calibration_value: float = None,
+            sensor_name: str = None):
+
+        self.sensor_model_id = sensor_model_id
+
+        self.tags = list(set(tags+gb_utils.get_tags_for_supported_sensor(sensor_model_id))) #append the tags from the file to the user defined tags
+        self.calibration_required = gb_utils.get_calibration_needed_for_supported_sensor(sensor_model_id)
+        self.sensor_model_name = sensor_name
+        self.get_value_function = get_value_function
+        self.calibration_function = calibration_function
+        self.calibration_value = calibration_value
+
+
+    def apply_calibration(self, *args, calibration_value):
+        return self.calibration_function(*args, calibration_value=calibration_value)
+
+    def get_value(self,*args):
+        if self.calibration_required:
+            if self.calibration_value:
+                return self.apply_calibration(self.get_value_function(*args),calibration_value=self.calibration_value) 
+            else:
+                raise Exception("This sensor needs a calibration value prior to reading values")
+        else:
+            return self.get_value_function(*args)
