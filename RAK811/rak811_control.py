cd5524b0c77b9d96a818f4cb53b57a726f750a14
PyroFourTwenty <kermitthecreeper@gmail.com>
2023-01-26 11:55:38 +0100

    added object to interface with ttn via rak811

---
+++ RAK811/rak811_control.py
@@ -0,0 +1,39 @@
+class RAK811():
+    ser = None
+    def __init__(self, port, baudrate=115200, timeout=3):
+        import serial
+        self.ser = serial.Serial(port, baudrate, timeout=timeout)
+        self.ser.reset_input_buffer()
+
+    def __send_at_command(self, command):
+        print('sending command:',command)
+        self.ser.write(str.encode(command+'\r\n'))
+        if command=='at+join' or 'at+send' in command:
+            print('waiting longer for join/send')
+            self.print_response(60)
+        else:
+            self.print_response()
+
+    def print_response(self, time_to_read = 2, return_on_ok = True):
+        from time import time
+        t = time()
+        while time()-t<time_to_read:
+            response = self.ser.readline().decode('utf-8').strip()
+            print('Response:',response)
+            if 'OK' in response and return_on_ok:
+                return
+
+    def send_lorawan_message(self, message, region, app_eui, app_key, dev_eui):
+        self.__send_at_command('at+set_config=device:sleep:0')
+        self.__send_at_command('at+set_config=device:restart')
+        self.__send_at_command('at+version')
+        self.__send_at_command('at+set_config=lora:join_mode:0')
+        self.__send_at_command('at+set_config=lora:class:0')
+        self.__send_at_command('at+set_config=lora:region:'+region)
+        self.__send_at_command('at+set_config=lora:app_eui:'+app_eui)
+        self.__send_at_command('at+set_config=lora:app_key:'+app_key)
+        self.__send_at_command('at+set_config=lora:dev_eui:'+dev_eui)
+        self.__send_at_command('at+set_config=device:restart')
+        self.__send_at_command('at+join')
+        self.__send_at_command('at+send=lora:2:'+message)
+
