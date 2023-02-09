15bc1003159ef1fe1abda28220c36357436e44e9
PyroFourTwenty <kermitthecreeper@gmail.com>
2023-02-10 00:17:24 +0100

    added calibration value retrieval from database

--- gb_headstation_utils.py
+++ gb_headstation_utils.py
@@ -1,5 +1,11 @@
 from GardenBusClient.SupportedSensors import supported_sensors
 import struct
+from flask_app import app
+from flask_sqlalchemy import SQLAlchemy
+from models import Station, Sensor, CalibrationValueForSensor
+
+db = SQLAlchemy(app)
+
 def number_to_bytes(number_ : int, number_of_bytes : int = 1, print_to_console:bool= False) -> bytearray:
     binary_node_id = bin(number_).replace("0b","")
     leading_zeros = "0" * (number_of_bytes*8-len(binary_node_id))
@@ -28,3 +34,18 @@ def bitstring_to_bytes(s):
 def binary(num):
     return ''.join('{:0>8b}'.format(c) for c in struct.pack('!f', num))
 
+def get_calibration_value_for_sensor_of_node(node_id, sensor_slot ):
+    with app.app_context():
+        try:
+            return CalibrationValueForSensor.query.filter_by(
+                belongs_to_sensor_id = Sensor.query.filter_by(belongs_to_station_id=node_id, station_slot=sensor_slot).first().id
+            ).first().calibration_value
+        except:
+            pass
+
+def get_model_id_of_sensor_of_node(node_id, sensor_slot):
+    with app.app_context():
+        try:
+            return Sensor.query.filter_by(belongs_to_station_id=node_id, station_slot=sensor_slot).first().sensor_model_id
+        except:
+            pass
